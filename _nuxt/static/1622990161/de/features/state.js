6edfcb57f08df6124af2bf7292bf42338de9c1be
D3SOX <D3SOX@users.noreply.github.com>
2021-06-06 14:56:12 +0000

    fix: improve error layout 76d215373c2acc2225a7ec8941dab3b9fb6157ff

--- _nuxt/static/1622990161/de/features/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622990161",layout:"default",error:a,state:{selectedOS:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fde\u002Ffeatures",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1622990161/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fde\u002Fdownload","\u002Fdownload","\u002Ffaq","\u002Ffeatures","\u002Fscreenshots","\u002Fwindows-setup","\u002Fde\u002Ffaq","\u002Fde\u002Ffeatures","\u002Fde\u002Fscreenshots","\u002Fde\u002Fwindows-setup","\u002Fde"]})
