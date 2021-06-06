76d215373c2acc2225a7ec8941dab3b9fb6157ff
D3SOX <d3sox@protonmail.com>
2021-06-06 16:54:06 +0200

    fix: improve error layout

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,21 +1,19 @@
 <template>
-  <v-app dark>
-    <v-main>
-      <v-container>
-        <v-card>
-          <v-card-title v-if="error.statusCode === 404">
-            {{ pageNotFound }}
-          </v-card-title>
-          <v-card-title v-else>
-            {{ otherError }}
-          </v-card-title>
-          <v-card-actions>
-            <NuxtLink to="/">Home page</NuxtLink>
-          </v-card-actions>
-        </v-card>
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title v-if="error.statusCode === 404">
+          {{ pageNotFound }}
+        </v-card-title>
+        <v-card-title v-else>
+          {{ otherError }}
+        </v-card-title>
+        <v-card-actions>
+          <NuxtLink to="/">Home page</NuxtLink>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
