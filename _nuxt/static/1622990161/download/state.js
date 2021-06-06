ea7f24fe67c98146d53d39ed49a04df42a4e7f5d
D3SOX <D3SOX@users.noreply.github.com>
2021-06-06 14:36:09 +0000

    refactor: fix some linter warnings 6a26b591a2755991b559def4347eaeb35e47434c

---
+++ _nuxt/static/1622990161/download/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622990161",layout:"default",error:a,state:{selectedOS:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fdownload",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
